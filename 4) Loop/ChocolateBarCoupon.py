7e785f9c314aefc5800a163930e26eebeab44c52
chrisstruong <chrisstruong@gmail.com>
2023-10-10 20:45:23 -0400

    Finish the assignment

---
+++ 4) Loop/ChocolateBarCoupon.py
@@ -0,0 +1,17 @@
+chocolate_expense = int(input("How much money would you like to buy chocolate? "))
+coupon = chocolate_expense
+
+total_chocolate = chocolate_expense + (coupon // 6)
+print(total_chocolate)
+coupon_remainder = total_chocolate % 6
+if coupon_remainder == 0:
+    total_chocolate += 1
+    coupon_remainder = total_chocolate % 6
+
+
+# Coupon after redem
+
+
+
+print(f"For {chocolate_expense} dollars, we could have consumed {total_chocolate} chocolate bars and still have {coupon_remainder} coupon(s) left")
+
---
+++ 4) Loop/SavingAccountTracker.py
@@ -0,0 +1,18 @@
+# 
+saving_balance = int(input("Enter your saving acccount balance: "))
+interest_rate = 0.05
+
+transaction = 0
+month_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+for month in month_list:
+    print(f"\033[1;36;40m{month}\033[0m: Your current balance is \033[1;36;40m${saving_balance:,.2f}\033[0m")
+    deposit_or_withdrawal = input(f"Would you like to deposit or withdrawal in {month} (d or w): ")
+    if deposit_or_withdrawal == 'd':
+        transaction = int(input(f"How much do you want to deposit? "))
+        saving_balance += transaction
+    else:
+        transaction = int(input(f"How much do you want to withdrawal? "))
+        saving_balance -= transaction
+    saving_balance +=  (saving_balance * interest_rate)
+    print()
