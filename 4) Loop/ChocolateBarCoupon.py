ed8c9e1ea9e86ce3ccfbb92c0e7b67f8f407b679
chrisstruong <chrisstruong@gmail.com>
2023-10-11 12:44:25 -0400

    Update chocolate bar component

--- 4) Loop/ChocolateBarCoupon.py
+++ 4) Loop/ChocolateBarCoupon.py
@@ -1,16 +1,15 @@
-print(5 // 6)
+# 4) Write a program to calculate additional chocolate bars
 chocolate_expense = int(input("How much money would you like to buy chocolate? "))
 coupon = chocolate_expense
 
 total_chocolate = chocolate_expense + (coupon // 6)
-print(total_chocolate)
-coupon_remainder = total_chocolate % 6
-if coupon_remainder == 0:
-    total_chocolate += 1
-    coupon_remainder = total_chocolate % 6
+coupon = coupon // 6 + coupon % 6 # Calculate coupon after redeming each time
+
+while coupon >= 6:
+    total_chocolate += coupon // 6 
+    coupon = coupon // 6 + coupon % 6
 
 
 
-
-print(f"For {chocolate_expense} dollars, we could have consumed {total_chocolate} chocolate bars and still have {coupon_remainder} coupon(s) left")
+print(f"For {chocolate_expense} dollars, we could have consumed {total_chocolate} chocolate bars and still have {coupon} coupon(s) left")
 
--- 4) Loop/ExamScore.py
+++ 4) Loop/ExamScore.py
@@ -1,4 +1,6 @@
 # 2) Write a program to read a list of exam scores given as integer percentages in the range 0 to 100
+
+# Input list of scores
 score_list = list(map(int, input("Enter a list of exam scores given as integer percentages in the range 0 to 100: ").split(' ')))
 score_length = len(score_list)
 
@@ -10,10 +12,12 @@ lowest = 100
 
 for score in score_list:
     total += score
+    # Find lowest and highest
     if score > highest:
         highest = score
     elif score < lowest:
         lowest = score
+    # Find grade types
     if score >= 90:
         A_total += 1
     elif score >= 80 and score < 90:
@@ -26,6 +30,8 @@ for score in score_list:
         F_total += 1
     
 average = round(total / score_length, 1)
+
+# Output
 print()
 print(f"Total number of scores = {len(score_list)}")
 print(f"Number of A's = {A_total}, {round((A_total / score_length) * 100)}%")
